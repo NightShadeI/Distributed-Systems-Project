1825c722a34573353d5c22b7ed6d281e952f3742
Avi Ram <abhinavs.ram@gmail.com>
2020-04-12 03:01:48 -0400

    Made more pythonic, cleaned up code

--- client.py
+++ client.py
@@ -5,41 +5,39 @@ import sys
 import xml.etree.ElementTree as ET
 
 def biggestserver(node):
-  if(node[0].tag == 'servers'):
-    biggest = int(node[0][0].attrib['coreCount'])
-    indexFinal = int(0)
-    tempIndex = int(0)
-    for child in node[0]:
-        if(int(child.attrib['coreCount']) > biggest):
-            indexFinal = tempIndex
-            biggest = int(child.attrib['coreCount'])
-        tempIndex+=1
-  return node[0][indexFinal].attrib['type']
+    if(node[0].tag == 'servers'):
+        biggest = int(node[0][0].attrib['coreCount'])
+        for child in enumerate(node[0]):
+            currchild = int(child[1].attrib['coreCount'])
+            if(currchild > biggest):
+                biggest = child        
+    return node[0][child[0]].attrib['type']        
+
 
 if __name__ == "__main__":
-  s = socket.socket()
-  s.connect(('127.0.0.1', 50000))
+    s = socket.socket()
+    s.connect(('127.0.0.1', 50000))
 
-  s.send('HELO'.encode())
-  data = s.recv(1024).decode()
-  if(data == 'OK'):
-    s.send('AUTH root'.encode())
+    s.send('HELO'.encode())
     data = s.recv(1024).decode()
+    if(data == 'OK'):
+        s.send('AUTH root'.encode())
+        data = s.recv(1024).decode()
+        s.send('REDY'.encode())
 
-  biggestServerName = biggestserver(ET.parse('system.xml').getroot())
-  start = True
+    biggestServerName = biggestserver(ET.parse('system.xml').getroot())
 
-  while (data == 'OK'):
-    if start:
-      s.send('REDY'.encode())
-      start = False
-    data = s.recv(1024).decode()
-    if data.split()[0] == 'JOBN':
-      s.send(('SCHD ' + data.split()[2] + ' ' + biggestServerName + ' 0').encode())
-      data = s.recv(1024).decode()
-      s.send('REDY'.encode())
-    elif data.split()[0] == 'NONE':
-      s.send('QUIT'.encode())
-      data = s.recv(1024).decode()
-      #test
-  s.close()
+    while (data == 'OK'):
+        data = s.recv(1024).decode()
+    
+        if data.startswith("JOBN"):
+            dataSend = ["SCHD",data.split()[2],biggestServerName,"0"]
+            dataSend = " ".join(dataSend)
+            s.send(dataSend.encode())
+            data = s.recv(1024).decode()
+            s.send('REDY'.encode())
+        elif data.startswith("NONE"):
+            s.send('QUIT'.encode())
+            data = s.recv(1024).decode()
+    
+    s.close()
