913c3eadc643973586411369f9d53a844f40a137
root <root@localhost.localdomain>
2020-04-12 02:27:44 -0400

    Made it more pythonic and changes ' -> "wq

--- client.py
+++ client.py
@@ -7,38 +7,37 @@ import xml.etree.ElementTree as ET
 def biggestserver(node):
   if(node[0].tag == 'servers'):
     biggest = int(node[0][0].attrib['coreCount'])
-    indexFinal = int(0)
-    tempIndex = int(0)
-    for child in node[0]:
-        if(int(child.attrib['coreCount']) > biggest):
-            indexFinal = tempIndex
-            biggest = int(child.attrib['coreCount'])
-        tempIndex+=1
-  return node[0][indexFinal].attrib['type']
+    for child in enumerate(node[0]):
+        currchild = int(child[1].attrib['coreCount'])
+        if(currchild > biggest):
+            biggest = child        
+  return node[0][child[0]].attrib['type']        
+
 
 if __name__ == "__main__":
   s = socket.socket()
   s.connect(('127.0.0.1', 50000))
 
-  s.send('HELO'.encode())
+  s.send("HELO".encode())
   data = s.recv(1024).decode()
-  if(data == 'OK'):
-    s.send('AUTH root'.encode())
+
+  if(data == "OK"):
+    s.send("AUTH root".encode())
     data = s.recv(1024).decode()
+    s.send("REDY".encode())
 
   biggestServerName = biggestserver(ET.parse('system.xml').getroot())
-  start = True
 
-  while (data == 'OK'):
-    if start:
-      s.send('REDY'.encode())
-      start = False
+  while (data == "OK"):
     data = s.recv(1024).decode()
-    if data.split()[0] == 'JOBN':
-      s.send(('SCHD ' + data.split()[2] + ' ' + biggestServerName + ' 0').encode())
+
+    if data.startswith("JOBN"):
+      dataSend = ["SCHD",data.split()[2],biggestServerName,"0"]
+      dataSend = " ".join(dataSend)
+      s.send(dataSend.encode())
       data = s.recv(1024).decode()
-      s.send('REDY'.encode())
-    elif data.split()[0] == 'NONE':
-      s.send('QUIT'.encode())
+      s.send("REDY".encode())
+    elif data.startswith("NONE"):
+      s.send("QUIT".encode())
       data = s.recv(1024).decode()
   s.close()
