73ed45af98e768a5eb36ec2549649f1702a69071
coopertim13 <cooper.timewell@gmail.com>
2020-05-10 20:45:18 +1000

    fixed (wow I need to learn git)

--- src/strategies/bestfit.py
+++ src/strategies/bestfit.py
@@ -1,11 +1,5 @@
 from strategies import strategy
 
 class BestFit(strategy.Strategy):
-<<<<<<< HEAD
 	def calculate(self, servers, job):
 		return servers[0][0]
-=======
-	def calculate_(self,jobParams,resourceInfo):
-	    
-            return resourceInfo[0][0]
->>>>>>> a601353810db32cef6bccb340a4630e7765a2a7f
