a601353810db32cef6bccb340a4630e7765a2a7f
root <root@localhost.localdomain>
2020-05-10 06:35:08 -0400

    best fit needs JobN details and Resource/Server info

--- src/strategies/bestfit.py
+++ src/strategies/bestfit.py
@@ -1,5 +1,6 @@
 from strategies import strategy
 
 class BestFit(strategy.Strategy):
-	def calculate_(self, jobParams):
-		return jobParams[0][0]
+	def calculate_(self,jobParams,resourceInfo):
+	    
+            return resourceInfo[0][0]
