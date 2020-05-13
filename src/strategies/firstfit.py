085e1aeb95b01e26dd2d43fcb65cb1f7d580bcd0
coopertim13 <cooper.timewell@gmail.com>
2020-05-13 14:08:50 +1000

    changed first fit

--- src/strategies/firstfit.py
+++ src/strategies/firstfit.py
@@ -2,4 +2,7 @@ from strategies import strategy
 
 class FirstFit(strategy.Strategy):
 	def calculate(self, servers, job):
+            for server in servers:
+                if(servers[server][3]!=-1):
+                    return servers[server]
             return servers[0]
