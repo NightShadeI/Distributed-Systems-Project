942862dde0fcc1e44e1d83f872375f5500be688a
Av1-R <abhinavs.ram@gmail.com>
2020-05-14 13:31:29 -0400

    Bored at 3am, edited run-comparison-test.sh

--- simulator/run-comparison-test.sh
+++ simulator/run-comparison-test.sh
@@ -5,23 +5,21 @@
 algorithm=$1
 trap "kill 0" EXIT
 
-echo "Running their implementation..."
+echo "Running their implementation and logging output in theirs.txt..."
 sleep 1
 ../ds-sim/ds-server -c configs/daconfig.xml -v all > theirs.txt&
 sleep 1
 ../ds-sim/ds-client -a $algorithm
-
+echo "Testing Complete!"
 echo "  "
-echo "Running our implementation..."
+echo "Running our implementation and logging output in ours.txt..."
 echo "# client.py 15-May-2020, 2020 Authored by: Avi.R, Cooper.T, Tom.T
-# Client started with '../src/client.py -a wf'"
+# Client started with '../src/client.py -a $algorithm'"
 sleep 2
 ./ds-server -c configs/daconfig.xml -v all > ours.txt&
 sleep 1
 python3 ../src/client.py -a $algorithm
 sleep 1
-
-echo "  "
 echo "Testing Complete!"
 
 TEST_OUTPUT=$false
@@ -31,12 +29,14 @@ sleep 1
 echo "  "
 echo "Checking Difference..."
 TEST_OUTPUT=$(diff <(echo "$OUT") EXPECTED_DIFF.txt)
-
+echo "  "
 sleep 1
 if [$TEST_OUTPUT != $false]
 then 
-	echo "Yay! :D THERE IS NO DIFFERENCE!"
+	cat success.txt
+	echo "You can the confirm results via log files -> theirs.txt and ours.txt files"
 else
-	echo "Oh No! D: THERE IS A DIFFERENCE!"
+	cat fail.txt
+	echo "You can the confirm the results via log files -> theirs.txt and ours.txt files"
 fi
 
