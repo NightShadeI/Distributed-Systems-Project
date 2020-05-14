2ac6a0ba1bbceafbe436f74d3a71d2dcf2cfeb1f
Av1-R <abhinavs.ram@gmail.com>
2020-05-14 12:01:28 -0400

    Commented the hell out of bf so that tutor cant take away my marks this time

--- src/strategies/bestfit.py
+++ src/strategies/bestfit.py
@@ -2,17 +2,37 @@ from strategies import strategy
 
 class BestFit(strategy.Strategy):
     def calculate(self, servers, job):
-        system = self.tree.getroot()
+
+        # Initialise variables which mark best-fit
         bestFit = 9999999
         minAvail = 9999999
         bfServer = servers[0]
-        for serverType in system[0]:
+
+        # Get system data (Read in via inherited class Strategy.py)
+        system = self.tree.getroot()
+
+        # Iterate server types in order of appearance in system.xml
+        for serverDefinitions in system[0]:
+            serverType = serverDefinitions.attrib["type"]
+
+            # Iterate all servers returned by ds-server response to RESC Capable
             for server in servers:
-                if(serverType.attrib["type"] == server.get_name()):
-                    if((server.cores_left(job) >= 0 and server.cores_left(job) < bestFit) or
+
+                #Check if name of current server matches server attribute in system.xml
+                if(server.get_name() == serverType):
+
+                    # If the first condition is true, bfServer satisfies "alt best fit" 
+                    #    -> (i.e. bfServer = best-fit Active server based on initial resource capacity)
+                    # OR
+                    # bfServer satisfies "best fit" condition
+                    #    -> (i.e. bfServer = best-fit server based on fitness value and minAvail time)
+                    if((server.can_run(job) and server.cores_left(job) < bestFit) or
                         (server.cores_left(job) == bestFit and server.get_available_time() < minAvail)):
+
+                        # If new bfServer or Active bfServer found, then reassign marker variables
                         bestFit = server.cores_left(job)
                         minAvail = server.get_available_time()
-                        bfServer = server  
+                        bfServer = server
 
         return bfServer
+
