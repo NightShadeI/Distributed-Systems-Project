83d6742976b3bc48bc888baf171e7105794691cb
coopertim13 <cooper.timewell@gmail.com>
2020-05-15 23:09:43 +1000

    final touches: without readme

--- simulator/test.py
+++
@@ -1,6 +0,0 @@
-import xml.etree.ElementTree as ET
-tree = ET.parse('system.xml')
-system = tree.getroot()
-servers = sorted(system[0], key=lambda x: int(x.attrib["coreCount"]))
-for item in servers:
-	print(item.attrib)
