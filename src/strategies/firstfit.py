73f852f3a127712085f142f96c5ca200aed66101
TapnerT17 <hontapner@gmail.com>
2020-05-15 12:42:37 +1000

    Finished firstfit.py

--- src/strategies/firstfit.py
+++ src/strategies/firstfit.py
@@ -1,13 +1,22 @@
+"""
+Distributed Systems group project
+Authors: Thomas Tapne
+Student ID: 45387168
+Practical session: Friday 10:00am
+"""
+
 from strategies import strategy
 
 
 class FirstFit(strategy.Strategy):
-    def calculate(self, servers, job):
-        system = self.tree.getroot()
-        for serverType in system[0]:
-            for server in servers:
-                if(serverType.attrib["type"] == server.get_name()):
-                    if(server.cores_left(job) >= 0):
-            	        return server
-        
-        return servers[0]
+	def calculate(self, servers, job):
+		# get the root element
+		system = self.tree.getroot()
+		# Iterate over each of the server types
+		for serverType in system[0]:
+			# Iterate over possible servers, only returning the first match of the server type that can run the job
+			for server in servers:
+				if(serverType.attrib["type"] == server.get_name() and server.can_run(job)):
+					return server
+		# If none capable of running job, just return the first server
+		return servers[0]
